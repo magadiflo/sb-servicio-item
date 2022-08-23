c05e6e99d8447880fa4016485d1261cf6030085e
Martín <magadiflo@gmail.com>
2022-08-23 17:47:33 -0500

    Creando controlador en items

---
+++ src/main/java/com/magadiflo/item/controller/ItemController.java
@@ -0,0 +1,33 @@
+package com.magadiflo.item.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.magadiflo.item.models.Item;
+import com.magadiflo.item.models.service.IItemService;
+
+@RestController
+@RequestMapping("/items")
+public class ItemController {
+
+	private final IItemService itemService;
+
+	public ItemController(IItemService itemService) {
+		this.itemService = itemService;
+	}
+
+	@GetMapping
+	public List<Item> listar() {
+		return this.itemService.findAll();
+	}
+
+	@GetMapping(path = "/producto/{id}/cantidad/{cantidad}")
+	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
+		return this.itemService.findById(id, cantidad);
+	}
+
+}
