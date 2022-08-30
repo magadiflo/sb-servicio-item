0ebc7693ea3657eb5572e7b9e0af1133ac513aec
Martín <magadiflo@gmail.com>
2022-08-29 20:08:12 -0500

    Configurando microservicio items con Resilience4J

--- src/main/java/com/magadiflo/item/controller/ItemController.java
+++ src/main/java/com/magadiflo/item/controller/ItemController.java
@@ -3,6 +3,7 @@ package com.magadiflo.item.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -13,16 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 import com.magadiflo.item.models.Item;
 import com.magadiflo.item.models.Producto;
 import com.magadiflo.item.models.service.IItemService;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 @RequestMapping("/items")
 public class ItemController {
 
 	private final IItemService itemService;
+	private final CircuitBreakerFactory cbFactory;
 
-	public ItemController(@Qualifier("serviceFeign") IItemService itemService) {
+	public ItemController(@Qualifier("serviceFeign") IItemService itemService, CircuitBreakerFactory cbFactory) {
 		this.itemService = itemService;
+		this.cbFactory = cbFactory;
 	}
 
 	@GetMapping
@@ -32,14 +34,12 @@ public class ItemController {
 		return this.itemService.findAll();
 	}
 
-	/**
-	 * @HystrixCommand, en caso falle la comunicación, le damos un camino alternativo llamando a otro método.
-	 * De esa manera evitamos errores en cascada que se puedan producir en nuestro ecosistema de microservicios.
-	 */
-	@HystrixCommand(fallbackMethod = "metodoAlternatrivo")
 	@GetMapping(path = "/producto/{id}/cantidad/{cantidad}")
 	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
-		return this.itemService.findById(id, cantidad);
+		//items, nombre que le daremos al cortocircuito
+		//Aplicamos resillience de forma programática
+		return cbFactory.create("items")
+				.run(() -> this.itemService.findById(id, cantidad), e -> metodoAlternatrivo(id, cantidad));
 	}
 	
 	public Item metodoAlternatrivo(Long id, Integer cantidad) {
