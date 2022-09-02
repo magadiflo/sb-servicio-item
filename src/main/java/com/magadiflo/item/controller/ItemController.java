931e6a2e2b51ed0c7922674da6414fd7b13e8e0b
Martín <magadiflo@gmail.com>
2022-09-01 19:28:26 -0500

    Implementando handler CRUD en controlador de servicio items

--- src/main/java/com/magadiflo/item/controller/ItemController.java
+++ src/main/java/com/magadiflo/item/controller/ItemController.java
@@ -14,11 +14,16 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.magadiflo.item.models.Item;
@@ -157,6 +162,24 @@ public class ItemController {
 		}
 		
 		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
+	}
+	
+	@PostMapping(path = "/producto")
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public Producto crear(@RequestBody Producto producto) {
+		return this.itemService.save(producto);		
+	}
+	
+	@PutMapping(path = "/producto/{id}")
+	@ResponseStatus(code = HttpStatus.OK)
+	public Producto editar(@RequestBody Producto producto, @PathVariable Long id) {
+		return this.itemService.update(producto, id);
+	}
+	
+	@DeleteMapping(path = "/producto/{id}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	public void eliminar(@PathVariable Long id) {
+		this.itemService.delete(id);
 	}
 
 }
